297d35defb08447d0aa83eff88a62272af0201b8
Romeo <romeovs@gmail.com>
2019-11-11 16:49:25 +0100

    Add source map support

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+import { install } from "source-map-support"
+install()
+
 import { clog, cerror, capture } from "../log/capture"
 import { Compilation } from "../compilation"
 
