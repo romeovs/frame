7ff6b88f383aea48aa7b6e0f659e33994e12d45e
Romeo <romeovs@gmail.com>
2019-11-12 08:21:30 +0100

    Add write helpers to compilation

--- src/compilation.js
+++ src/compilation.js
@@ -1,6 +1,12 @@
+import path from "path"
+
 import printf from "printf"
+
 import { type Config } from "./config"
 import { ColorfulLogger, LeveledLogger, NoopLogger, SimpleLogger } from "./log"
+import { build } from "./build"
+import { hash } from "./hash"
+import fs from "./fs"
 
 export class Compilation {
 	config : Config
@@ -48,6 +54,76 @@ export class Compilation {
 	}
 
 	run () {
-		this.fatal("NOT IMPLEMENTED")
+		return build(this)
+	}
+
+	get cachedir () : string {
+		return path.resolve(this.config.root, ".frame_cache")
+	}
+
+	get outputdir () {
+		return path.resolve(this.config.output)
+	}
+
+	// Write the contents of the file to the output folder, performing
+	// some other transformations that might need to be done (like gzipping).
+	async write (filename : string, content : string | Buffer, addHash : boolean = false) : Promise<string> {
+		let fname = filename
+		if (addHash) {
+			const ext = path.extname(filename)
+			const base = filename.replace(new RegExp(`${ext}$`), "")
+			fname = `${base}.${await hash(content)}${ext}`
+		}
+
+		this.log("Writing %s", fname)
+
+		await Promise.all([
+			this._writeFile(fname, content),
+			// gzip(this, fname, content),
+			// brotli(this, fname, content),
+		])
+
+		return fname
+	}
+
+	// Write a file to the output folder
+	async _writeFile (filename : string, content : string | Buffer) {
+		const fn = path.resolve(this.outputdir, filename.replace(/^\//, ""))
+		const dir = path.dirname(fn)
+
+		await fs.mkdir(dir, { recursive: true })
+		await fs.writeFile(fn, content)
+	}
+
+	_resolveOutputFile (filename : string) : string {
+		return path.resolve(this.config.output, filename.replace(/^\//, ""))
+	}
+
+	async copy (src : string, dst : string) : Promise<string> {
+		const fname = this._resolveOutputFile(dst)
+		const dir = path.dirname(fname)
+
+		this.log("Writing %s", fname)
+
+		await fs.mkdir(dir, { recursive: true })
+		await fs.copyFile(src, fname)
+
+		return fname
+	}
+
+	read (...segments : string[]) : Promise<string> {
+		return fs.readFile(path.resolve(...segments), "utf-8")
+	}
+
+	async writeCache (filename : string, content : string | Buffer) : Promise<string> {
+		const fname = path.resolve(this.cachedir, filename.replace(/^\//, ""))
+		const dir = path.dirname(fname)
+
+		this.debug("Writing cache %s", fname)
+
+		await fs.mkdir(dir, { recursive: true })
+		await fs.writeFile(fname, content)
+
+		return fname
 	}
 }
