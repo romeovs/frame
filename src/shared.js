f31dfc7789d06b40a84247e423557ac362064d48
Romeo <romeovs@gmail.com>
2019-11-12 09:22:37 +0100

    Add shared plugins for client/server comilation

---
+++ src/shared.js
@@ -0,0 +1,33 @@
+import postcss from "rollup-plugin-postcss"
+import { hash } from "./hash"
+import { format } from "./timer"
+
+type Timings = {
+	string : [ number, number, number ],
+}
+
+export function print (ctx : Compilation, pfx : string, timings : Timings) {
+	for (const key in timings) {
+		const t = timings[key]
+		ctx.debug(`${pfx} %s (%s)`, key, format(Math.round(t[0])))
+	}
+}
+
+export function plugins (ctx : Compilation) {
+	return [
+		postcss({
+			modules: {
+				generateScopedName (classname : string, filename : string, css : string) : string {
+					return `${classname}_${hash(css)}`
+				},
+			},
+			extract: true,
+			inject: true,
+			sourceMap: true,
+			minimize: !ctx.config.dev,
+			plugins: [
+				// TODO
+			],
+		}),
+	]
+}
