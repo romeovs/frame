64b5f57f3309b57d81785f11b79e92f8ac7cd8ca
Romeo <romeovs@gmail.com>
2019-11-15 11:46:11 +0100

    Add loggin to serve

--- src/serve.js
+++ src/serve.js
@@ -1,8 +1,19 @@
 import express from "express"
 
+import Timer from "./timer"
+
 export function serve (ctx : Compilation) {
-	// Set up express server
+	const port = ctx.config.port || 8080
+
 	const app = express()
+	app.use(function (req, res, next) {
+		const timer = new Timer()
+		next()
+		ctx.log("  GET %s %s (%s)", res.statusCode, req.path, timer)
+	})
 	app.use(express.static(ctx.outputdir))
-	app.listen(ctx.config.port || 8080)
+
+	app.listen(port)
+
+	ctx.log("Listening on %s", `http://localhost:${port}`)
 }
